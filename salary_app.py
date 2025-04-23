09451b3eadd763e94a14a4990a762209aea4b2a3
jpatel355 <jpatel35@ncsu.edu>
2025-04-23 17:52:24 -0400

    Update salary_app.py

--- salary_app.py
+++ salary_app.py
@@ -3,7 +3,7 @@ import pickle
 import pandas as pd
 
 # Load the trained model
-with open("/content/kaggle2022_model.pkl", "rb") as f:
+with open("kaggle2022_model.pkl", "rb") as f:
     model = pickle.load(f)
 
 # App title and subtitle
